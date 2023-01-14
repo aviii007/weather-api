8dc4a6ede63296f74191b31f79319eb115d3f7a6
Ashvini Kanojia <99378217+aviii007@users.noreply.github.com>
2023-01-14 12:21:19 +0530

    Update WeatherController.java

--- src/main/java/com/weather/api/Controller/WeatherController.java
+++ src/main/java/com/weather/api/Controller/WeatherController.java
@@ -26,7 +26,7 @@ public class WeatherController {
     public WeatherForecast forecast(@PathVariable String location) {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
-        headers.set("X-RapidAPI-Key", "3fe421aa57mshdfd4147f4d4c319p1ba7f4jsnca7d59efce2d");
+        headers.set("X-RapidAPI-Key", "Your_Api_Key");
         headers.set("X-RapidAPI-Host", "visual-crossing-weather.p.rapidapi.com");
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
